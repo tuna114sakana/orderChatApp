c42c3301eedeb89619742a31ebb7f580c79df559
山田良樹 <yoshiki@Air.local>
2020-01-10 22:44:06 +0900

    function of chat created

--- orderChatApp/app/javascript/channels/room_channel.js
+++ orderChatApp/app/javascript/channels/room_channel.js
@@ -10,9 +10,9 @@ const chatChannel = consumer.subscriptions.create("RoomChannel", {
   },
 
     received: function(data) {
-    // Called when there's incoming data on the websocket for this channel
-        return alert(data['message']);
+        return $('#messages').append(data['message']);
   },
+
 
   speak: function(message) {
     return this.perform('speak', {message: message});
--- orderChatApp/app/models/message.rb
+++ orderChatApp/app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ApplicationRecord
+  validates :content, presence: true
+  #createの後にコミットする [ MessageVroadcastJobのperformを遅延実行 引数はself
+  after_create_commit { MessageBroadcastJob.perform_later self }
 end
